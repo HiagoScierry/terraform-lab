c940dd988cf3caf2bed1cad177b6d976691893f7
Hiago Moreira <hiagocarlosmoreira@hotmail.com>
2025-09-06 18:19:53 -0300

    organize repo

--- ec2/output.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Endereço IP público da instância EC2."
-  value       = aws_instance.web_server.public_ip
-}
-
-output "public_dns" {
-  description = "DNS público da instância EC2."
-  value       = aws_instance.web_server.public_dns
-}
